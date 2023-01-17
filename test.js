427e2375387df0d6f11212bcd53075e1aea42390
dany990011 <115802827+dany990011@users.noreply.github.com>
2023-01-17 21:05:46 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,115 @@
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+import fontAwesome from "https://cdn.skypack.dev/font-awesome@4.7.0";
+import {faSquareTwitter} from "https://cdn.skypack.dev/@fortawesome/free-brands-svg-icons@6.2.1";
+
+
+
+let data = {}
+
+const url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+async function getRawQuotes(){
+  const response = await fetch(url)
+  data = await response.json()
+  
+}
+getRawQuotes()
+
+//REDUX
+
+
+
+//REACT
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      quotes : "Loading",
+      currentQuote: "",
+      sameQuoteTest: "0"
+    }
+    this.refresh = this.refresh.bind(this);
+    this.test = this.test.bind(this);
+  }
+ 
+  componentDidMount(){
+    
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({quotes: JSON.stringify(data)});
+        let rawQuotes = data.quotes;
+        let oneQuote = rawQuotes[Math.floor(Math.random()*rawQuotes.length)];
+        this.setState({currentQuote: oneQuote.quote })
+        this.setState({currentAuthor: oneQuote.author })
+      })
+      
+      
+      
+     
+    
+  }
+  refresh(){
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+      const rawQuotes = data.quotes
+      let newQuote = "";
+      
+      newQuote = rawQuotes[Math.floor(Math.random()*rawQuotes.length)]
+      
+      
+      if (newQuote.quote == this.state.currentQuote || this.state.sameQuoteTest == "1"){
+        console.log("SAME QUOTE DETECTED, REFRESHING AGAIN")
+        this.setState({sameQuoteTest : "0"})
+        this.refresh()
+      }else {
+        this.setState({currentQuote : newQuote.quote})
+        this.setState({currentAuthor : newQuote.author})
+      }
+      })
+
+  }
+  test(){
+    if(this.state.sameQuoteTest == 0){
+      this.setState({sameQuoteTest : "1"})
+    }else (this.setState({sameQuoteTest : "0"}))
+    
+  }
+ 
+    
+    
+  
+  
+   render(){
+    return(
+      <div className = "Wrapper" id="quote-box">
+        <div id = "text-and-author">
+        <div id = "text-box">
+        <h2 id = "text"> {this.state.currentQuote}</h2>
+        </div>
+        <div id = "author-box">
+        <h3 id = "author"> - {this.state.currentAuthor}</h3>
+        </div>
+        </div>
+        <div id = "buttons">
+        <button onClick = {this.refresh} id = "new-quote">New Quote</button>
+        {/*<button onClick = {this.test}>TEST : {this.state.sameQuoteTest}</button>*/}
+        
+        </div>
+        <div id="tweet-quote-padding">
+        <a id="tweet-quote" href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+ '"' + this.state.currentQuote + '" ' + " -" + this.state.currentAuthor}>Tweet <i class="fa fa-twitter" ></i></a>
+       
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+class AppWrapper extends React.Component {
+
+ 
+};
+
+ReactDOM.render(<App/>,document.getElementById("root"))
