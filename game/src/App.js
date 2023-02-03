95e7f2ddcc5baf3ab8a45826a3cb74a189e4094b
TwoLines0 <twolines0@gmail.com>
2023-02-03 19:19:41 +0000

    Update App.js

--- game/src/App.js
+++ game/src/App.js
@@ -1,22 +1,31 @@
 
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  
+  const [position, setPosition] = useState({left : 0, top: 0})
+  const [target, steTarget] = useState({left : document.getElementById("target").style.left})
   
   
   useEffect(()=>{
-    setInterval(frame , 1000)
+    setInterval(frame , 100)
   },[])
 
 
   function frame(){ 
-    const seeker = document.getElementById("seeker")
-    const target = document.getElementById("target")
-    console.log(seeker.style.left);
-    seeker.style.left = 100;
-    seeker.style.top = 100;
+    //const seeker = document.getElementById("seeker")
+    //const target = document.getElementById("target")
+    setPosition((prev)=>{
+      return{
+
+        left: prev.left + target,
+        top: prev.top + 3
+
+      }
+    })
+    //console.log(seeker.style.left);
+    //seeker.style.left = 100;
+    //seeker.style.top = 100;
   }
   return (
     <div className="App">
@@ -24,7 +33,7 @@ function App() {
         Game Project
       </header>
       <div className='world'>
-        <div id="seeker" className='seeker'></div>
+        <div id="seeker" className='seeker' style={{left: position.left, top: position.top}}></div>
         <div id="target" className='target'></div>
       </div>
     </div>
