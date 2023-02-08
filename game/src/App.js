78ec9a72675a74c63b4b6ba49cfc09b0b66aeade
TwoLines0 <twolines0@gmail.com>
2023-02-08 00:50:07 +0000

    added working 25 + 5 timer

--- game/src/App.js
+++ game/src/App.js
@@ -3,43 +3,95 @@ import './App.css';
 import { useEffect, useRef, useState } from 'react';
 
 function App() {
-
-  const [position, setPosition] = useState({left : 0, top: 0})
-  const [x , setX] = useState()
-  const [y , setY] = useState()
+  const minWidth = document.querySelector("html").getBoundingClientRect().left
+  const maxWidth = document.querySelector("html").getBoundingClientRect().width
+  const maxHeight = document.querySelector("html").getBoundingClientRect().height
+  
+  const [position, setPosition] = useState({left : Math.random()*(maxWidth) + minWidth, top: Math.random()*(maxHeight)})
   const targetRef = useRef()
+  const seekerRef = useRef()
 
-
-  useEffect(()=>{
-    const getX = targetRef.current.offsetLeft
-    setX(getX)
-    const getY = targetRef.current.offsetTop
-    setY(getY)
-
-  },[])
-  console.log(typeof(position.left) )
-  setInterval(frame , 1000)
+  let loseCondition = false;
   
 
-  function frame(){ 
+  useEffect(()=>{
+    
+    const targetX = targetRef.current.getBoundingClientRect().left
+    const targetY = targetRef.current.getBoundingClientRect().top
+    
+    
+    
+    const intervalId = setInterval(()=>{
+      if (loseCondition == false){
 
-    setPosition((prev)=>{
-      console.log("here is x in the setPosition "+x)
-      return{
+        setPosition((prev)=>{
 
-        left:prev.left + x,
-        top:y*0.1
+          //const distance = Math.sqrt(
+          //  (targetX - prev.left) ** 2 + (targetY - prev.top) ** 2
+          //);
+        
+          return{
 
+            //left: prev.left + 2 * Math.cos(angle),
+            //top: prev.top + 2 * Math.sin(angle),
+    
+            left:(prev.left + (targetX-prev.left)*0.01),
+            top:(prev.top + (targetY-prev.top)*0.01)
+    
+          }
+        })
       }
-    })
+      return () => clearInterval(intervalId);
+
+    } , 10)
+
+    
+    //setInterval(()=>{console.log(targetX+", "+targetY)} , 100)
+    console.log(loseCondition)
+    const refreshRate = setInterval(()=>{checkSeekerTarget()} , 10)
+
+    
+
+    const checkSeekerTarget = () => {
+      if(
+        ((((seekerRef.current.getBoundingClientRect().left ) < (targetRef.current.getBoundingClientRect().left))&&
+        ((seekerRef.current.getBoundingClientRect().left ) > (targetRef.current.getBoundingClientRect().left-50))) ||
+        (((seekerRef.current.getBoundingClientRect().left ) > (targetRef.current.getBoundingClientRect().left))&&
+        ((seekerRef.current.getBoundingClientRect().left ) < (targetRef.current.getBoundingClientRect().left+50))))
+        &&
+        ((seekerRef.current.getBoundingClientRect().top) > (targetRef.current.getBoundingClientRect().top-50))
+  
+        ){
+        
+        
+        //setPosition(()=>{
+        //  return{
+        //    left : Math.random()*(maxWidth) + minWidth, 
+        //    top: Math.random()*(maxHeight)
+        //  }
+        //})
+        alert('You Lose!')
+        loseCondition = true
+        console.log(loseCondition)
+        return () => clearInterval(refreshRate)
+      }
+    }
+  },[])
+  
+  const reset = () =>{
+    loseCondition = false
+    console.log(loseCondition)
   }
+  
+
   return (
     <div className="App">
       <header className="App-header">
         Game Project
       </header>
+      <div><button onClick={reset} >Reset</button></div>
       <div className='world'>
-        <div id="seeker" className='seeker' style={{left: position.left, top: position.top}}></div>
+        <div id="seeker" className='seeker' ref={seekerRef} style={{left: position.left, top: position.top}}></div>
         <div id="target" className='target' ref={targetRef}></div>
       </div>
     </div>
--- timer/src/App.js
+++ timer/src/App.js
@@ -1,20 +1,178 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>
-          Timer Project
-        </h1>
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      display: "session",
+      timerRunning: false,
+      sessionLength : 25,
+      breakLength : 5,
+      currentMinutes : 25,
+      currentSecounds : "00",
+    }
+    this.upSessionLength = this.upSessionLength.bind(this)
+    this.downSessionLength = this.downSessionLength.bind(this)
+    this.upBreakLength = this.upBreakLength.bind(this)
+    this.downBreakLength = this.downBreakLength.bind(this)
+    this.resetTimer = this.resetTimer.bind(this)
+    this.startStopTimer = this.startStopTimer.bind(this)
+    this.audio = ""
+    
+  }
 
-        <div></div>
-        
-      </header>
-    </div>
-  );
+  
+  componentDidMount(){
+    this.secoundsCounter()
+  }
+
+
+  secoundsCounter(){
+    this.audio = document.getElementById("beep")
+    this.audio.volume = 0.3
+    if(this.state.timerRunning){
+    
+      this.timer = setTimeout(()=>{
+        console.log((this.state.currentSecounds-1+'').length)
+        if((this.state.currentSecounds-1+'').length < 2){
+          this.setState({currentSecounds: "0"+(this.state.currentSecounds-1)})
+          console.log("less than 2")
+          this.secoundsCounter()
+          return
+        }
+        this.setState({currentSecounds: this.state.currentSecounds-1})
+        this.secoundsCounter()
+      },10)
+
+      if((this.state.currentSecounds <= 1)&&(this.state.currentMinutes <= 0)&&(this.state.display != "break")){
+        this.setState({timerRunning: false})
+        clearTimeout(this.timer)
+        this.audio.play()
+        this.startBreak()
+        return
+      }
+
+      if((this.state.currentSecounds <= 1)&&(this.state.currentMinutes <= 0)&&(this.state.display == "break")){
+        this.setState({timerRunning: false})
+        clearTimeout(this.timer)
+        this.audio.play()
+        this.startSession()
+        return
+      }
+
+      if(this.state.currentSecounds == 0){
+        this.minuteCounter()
+        return
+      }
+    }else{
+      clearTimeout(this.timer)
+    }
+    return
+  }
+
+  minuteCounter(){
+    if((this.state.currentMinutes-1+'').length < 2){
+      this.setState({currentMinutes: "0"+(this.state.currentMinutes-1), currentSecounds: 59})
+      return
+    }
+    this.setState({currentMinutes: this.state.currentMinutes-1, currentSecounds: 59}) 
+  }
+
+  startStopTimer(){
+    
+    this.setState({timerRunning: !this.state.timerRunning}, () => this.secoundsCounter())
+    
+  }
+
+  resetTimer(){
+    this.setState({timerRunning: false,sessionLength:25, currentMinutes: 25, currentSecounds: "00", breakLength: 5, display: "session"})
+    clearTimeout(this.timer)
+    this.audio.pause()
+    this.audio.currentTime = 0
+  }
+
+  upSessionLength(){
+    if(this.state.timerRunning){
+      return
+    }
+    this.setState({sessionLength: this.state.sessionLength+1,currentMinutes: this.state.sessionLength+1})
+  }
+
+  downSessionLength(){
+    if(this.state.timerRunning){
+      return
+    }
+    this.setState({sessionLength: this.state.sessionLength-1,currentMinutes: this.state.sessionLength-1})
+  }
+
+  upBreakLength(){
+    if(this.state.timerRunning||(this.state.breakLength >= 60)){
+      return
+    }
+    this.setState({breakLength: this.state.breakLength+1})
+  }
+
+  downBreakLength(){
+    if(this.state.timerRunning||(this.state.breakLength <= 1)){
+      return
+    }
+    this.setState({breakLength: this.state.breakLength-1})
+  }
+
+  startBreak(){
+    this.setState({currentMinutes: this.state.breakLength, currentSecounds: "00", timerRunning:true, display: "break"}, () => this.secoundsCounter())
+  }
+
+  startSession(){
+    this.setState({currentMinutes: this.state.sessionLength, currentSecounds: "00", timerRunning:true, display: "session"}, () => this.secoundsCounter()) 
+  }
+
+  
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <h1>
+            Timer Project
+          </h1>
+
+          <div>
+            
+            <div>
+              <h2 id="session-label">Session length:  {this.state.sessionLength}</h2>
+              <h2 id="break-label">Break length:  {this.state.breakLength}</h2>
+              <h1 id="time-left">{this.state.currentMinutes} : {this.state.currentSecounds}</h1>
+              <h3 id="timer-label">status: {this.state.display}</h3>
+              <audio id="beep" src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"></audio>
+            </div>
+            <div>
+              <button id="reset" onClick={this.resetTimer}>Reset</button>
+              <div>
+                <button id="session-decrement" onClick={this.downSessionLength}>Decrease session length</button>
+                <button id="session-increment" onClick={this.upSessionLength}>increase session length</button>
+              </div>
+              <div>
+                <button id="break-decrement" onClick={this.downBreakLength}>Decrease break length</button>
+                <button id="break-increment" onClick={this.upBreakLength}>increase break length</button>
+              </div>
+              <div>
+                <button id="start_stop" onClick={this.startStopTimer}>Start/Stop</button>
+              </div>
+            </div>
+          </div>
+          
+        </header>
+      </div>
+    );
+  }
 }
+
+
+
 
 export default App;
